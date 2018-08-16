0cb8c30d67bb58490cc886fc20590f15226ea3f6
tanagromo <tanagromo@gmail.com>
2018-08-15 20:38:54 -0500

    tarea1 tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,16 @@
 def tokenizer(file):
+    lista = []
+    provisional = []
+    for palabra in [item for item in open(file,"r").read().replace("\n","").split(" ") if item != ""]:
+        for letra in palabra:
+            if letra in ['{', '}', '(', ')', ';']:
+                lista.append(letra)
+            else: 
+                provisional.append(letra)
+        if len(provisional) != 0:
+            lista.append("".join(provisional))
+        provisional = []
+    return lista
 
-    l = open(file,"r").read()
-    print(l)
-
-tokenizer("tarea1.c")
+print(tokenizer("tarea1.c"))
 
