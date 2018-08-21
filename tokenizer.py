8e334271073533ab6d0e12faac03e7aacae6d4ad
tanagromo <tanagromo@gmail.com>
2018-08-21 12:58:05 -0500

    add keywords

--- tokenizer.py
+++ tokenizer.py
@@ -1,19 +1,15 @@
-def tokenizer(file):
-    lista = []
-    provisional = []
-    for palabra in [item for item in open(file,"r").read().replace("\n","").split(" ") if item != ""]:
-        for letra in palabra:
-            if letra in ['{', '}', '(', ')', ';','\.','#']:
-                if len(provisional) != 0:
-                    lista.append("".join(provisional))
-                lista.append(letra) 
-                provisional=[]
-            else: 
-                provisional.append(letra)
-        if len(provisional) != 0:
-            lista.append("".join(provisional))
-        provisional = []
-    return lista
+def tokenizer(file,key):
+	tokens = {'(':'Open parenthesis', ')': 'Close parenthesis', '{': 'Open brace', '}': 'Close brace', ';': 'Semicolon'}
+	#las key words se pueden leer de un archivo
+	key={'auto','break','case','char','const','continue','default','do','double','else','enum','extern','float',
+            'for','if','inline','int','long','register','restrict','return','sizeof','static','struct','switch',
+            'typedef','union','unsigned','void','volatile','while'}
+	program = open(file, 'r').read().replace("\n", "").replace("\t", "")
+	for x in tokens:
+	    program = program.replace(x, ' '+x+' ')
+        for y in key:
+            program = program.replace(y,'Keyword'+'<'+y+'>')
+	return [item+'' for item in program.split(" ") if len(item) != 0]
 
-print(tokenizer("tarea1.c"))
+print(tokenizer('tarea1.c','key.txt'))
 
