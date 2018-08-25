f742f9fa1c33fc4727fa192599ff9eebe35beb94
tanagromo <tanagromo@gmail.com>
2018-08-24 22:56:56 -0500

    agregando etiquetas

--- tokenizer.py
+++ tokenizer.py
@@ -1,14 +1,14 @@
 def tokenizer(file):
-	tokens = {'(':'Open parenthesis', ')': 'Close parenthesis', '{': 'Open brace', '}': 'Close brace', ';': 'Semicolon'}
+	tokens = {'(':'OpenParen', ')': 'CloseParen', '{': 'OpenBrace', '}': 'CloseBrace', ';': 'Semicolon'}
 	#las key words se pueden leer de un archivo
 	key={'auto','break','case','char','const','continue','default','do','double','else','enum','extern','float',
             'for','if','inline','int','long','register','restrict','return','sizeof','static','struct','switch',
             'typedef','union','unsigned','void','volatile','while'}
 	program = open(file, 'r').read().replace("\n", "").replace("\t", "")
 	for x in tokens:
-	    program = program.replace(x, ' '+x+' ')
+	    program = program.replace(x, ' '+tokens[x]+'<'+x+'>'+' ')
         for y in key:
-            program = program.replace(y,'Keyword'+'<'+y+'>')
+            program = program.replace(y,' Keyword'+'<'+y+'> ')
 	return [item+'' for item in program.split(" ") if len(item) != 0]
 
 print(tokenizer('tarea1.c'))
