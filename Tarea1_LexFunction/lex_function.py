c0ef63712f1b37a55350df0be60044966df67102
Gerardo Arjona <gerardo.arj15@gmail.com>
2018-08-26 11:37:53 -0500

    Add any number length validation
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

--- Tarea1_LexFunction/lex_function.py
+++ Tarea1_LexFunction/lex_function.py
@@ -1,14 +1,60 @@
 import re
+from enum import Enum
+
+#class Token(Enum):
+#    OpenBrace="{",
+#    CloseBrace="}",
+#    OpenParen="(",
+#    CloseParen=")",
+#    Semicolon=";",
+#    IntId="int",
+
 
 def lex():
     tokens=[]
     word=""
     file=open("return_2.c", "r")
+    
     for line in file:
         l=line.replace(" ", "").strip("\n")
-        numero_searched=re.search(r'[0-9]+\;',l)
-        if  numero_searched:
-            tokens.append(numero_searched.group(0).strip(';'))
+        for letter in l:
+            word=word+letter
+            word=word.strip("()\{\};")
+            if(word=="int"):
+                tokens.append("INT_ID<"+word+">")
+                word=""
+            elif(word=="main"):
+                tokens.append("ID<"+word+">")
+                word=""
+            elif(word=="return"):
+                tokens.append(word.upper())
+                word=""
+            elif(letter not in "int" and letter not in "return" and letter not in "main"):
+                single_numero_searched=re.search(r'[0-9]',letter)
+                if  single_numero_searched:
+                    numero_searched=re.search(r'[0-9]+\;',l)
+                    if  numero_searched:
+                        if ("INT<"+numero_searched.group(0).strip(';')+">" not in tokens):
+                            tokens.append("INT<{}>".format(numero_searched.group(0).strip(';')))
+                else:
+                    tok=re.search(r'[\{ | \} | \( | \) | \;]',letter)
+                    if tok:
+                        tokens.append(tok.group(0))
+
+
+#    for line in file:
+#        l=line.replace(" ", "").strip("\n")
+#        int_searched=re.search(r'int',l)
+#        if  int_searched:
+#            tokens.append("INT<"+int_searched.group(0)+">")
+#        fun_name_searched=re.search(r'int[a-z]+\(',l)
+#        if  fun_name_searched:
+#            tokens.append("ID<"+int_searched.group(0)+">".strip("int("))
+#        for letter in line:
+#            if(letter=="{" or letter="}" or letter"(" or letter")" or ";")
+#            numero_searched=re.search(r'[0-9]+\;',l)
+#            if  numero_searched:
+#                tokens.append(numero_searched.group(0).strip(';'))
 
     print(tokens)
     return tokens
