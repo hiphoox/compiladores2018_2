754aa02452cd761f1b1216fd9378250e3c1b1769
DevelopMec <42444447+DevelopMec@users.noreply.github.com>
2018-08-25 23:43:09 -0500

    Add files via upload

---
+++ braceType.java
@@ -0,0 +1,13 @@
+public class braceType
+{
+	private String openBrace;
+	private String closeBrace;
+	private String openCurlyBrace;
+	private String closeCurlyBrace;
+	
+
+	public braceType(String brace)
+	{
+		
+	}	
+}
