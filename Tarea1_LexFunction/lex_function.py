08f1f0523caf60887730c569cba44d509ae926a9
Gerardo Arjona <gerardo.arj15@gmail.com>
2018-08-26 10:07:23 -0500

    Add First Regular Expresion
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

--- Tarea1_LexFunction/lex_function.py
+++ Tarea1_LexFunction/lex_function.py
@@ -6,21 +6,10 @@ def lex():
     file=open("return_2.c", "r")
     for line in file:
         l=line.replace(" ", "").strip("\n")
-        _l=l
-        for letter in l:
-            word=word+letter
-            word=word.strip("()\{\};")
-            if(word=="int"):
-                tokens.append("INT<"+word+">")
-                word=""
-            elif(word=="main"):
-                tokens.append("ID<"+word+">")
-                word=""
-            elif(word=="return"):
-                tokens.append(word)
-                word=""
-            elif(letter not in "int" and letter not in "return" and letter not in "main"):
-                tokens.append(letter)
+        numero_searched=re.search(r'[0-9]+\;',l)
+        if  numero_searched:
+            tokens.append(numero_searched.group(0).strip(';'))
+
     print(tokens)
     return tokens
             
