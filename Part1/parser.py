b059a8139b5959e2c63d6d4878bc2d3e0d241cf9
Gerardo Arjona <gerardo.arj15@gmail.com>
2018-08-31 20:13:56 -0500

    Add first aproach parser
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

--- Part1/parser.py
+++ Part1/parser.py
@@ -1,11 +1,46 @@
 import re
 import os
 from lex_function import lex
-from enum import Enum
+
+def expression(ret):
+    num=[]
+    for l in ret:
+        numero_searched=re.search(r'[0-9]+',l)
+        if(numero_searched):
+            num.append(numero_searched.group(0))
+            return num
+
+def statement(tokens):
+    stat=[]
+    for t in tokens:
+        numero_searched=re.search(r'INT<[0-9]+>',t)
+        if(numero_searched):
+            stat.append(expression(numero_searched.group(0)))
+            tokens.remove(t)
+    return stat
+
+def program(tokens):
+    p=[]
+    for t in tokens:
+        if(t=="INT_ID<int>"):
+            p.append(t)
+            tokens.remove(t)
+        elif(t=="ID<main>"):
+            p.append(t)
+            tokens.remove(t)
+        else:
+            p.append(statement(tokens))
+    return p
 
 def parse(tokens):
+    ast=[]
+    ast.append(program(tokens))
+    print(ast)
+    return ast
 
-    print("hola")
+
+
+
 
 
 parse(lex())
