9abbe0706c2f17d835c29ee2f10cc36121a2e8fa
ArmandoVn <armandoeloriginal@live.com.mx>
2018-09-02 10:48:09 -0500

    - Se definieron simbols, keyWords e indentifiers diccionarios cuyo proposito es identificar y etiquetar elementos dentro del codigo.

--- Tarea 1 lex/lex.py
+++ Tarea 1 lex/lex.py
@@ -1,30 +1,45 @@
-from definitions import tokens, keyWords
-
 def lex(fileName):
 	file = open(fileName, "r")
 	text = ''.join(file.read().split())
 
-	tokens = '(){};'
-	keyWords = dict()
+	simbols = {
+		'(' : 'openParentesis',
+		')' : 'closeParentesis',
+		'{' : 'openBrace',
+		'}' : 'closeBrace',
+		';' : 'semicolon'
+	}
 	keyWords = {
 		'int' : 'intKeyWord',
-		'return' : 'returnKeyWord',
-		'main'	: 'functionKeyWord'
+		'return' : 'returnKeyWord'
 	}
+	identifiers = {
+		'main' : 'identifier'
+	}
+
 	string = ''
 	tokensList = []
 
+	#for que itera en cada uno de los elementos de la cadena 'text'.
 	for x in text:
-		if tokens.find(x) == -1:
+		#Realiza la busqueda de alguna coincidencia con el diccionario tokens
+		key = simbols.get(x)
+		if key == None:
 			string += x
-			key = keyWords.get(string)
-			if key != None:
-				tokensList.append(key+'('+string+')')
+			identifier = identifiers.get(string)
+			keyWord = keyWords.get(string)
+			if identifier != None:
+				tokensList.append(identifier + '('+string+')')
+				string=''
+			elif keyWord != None:
+				tokensList.append(keyWord)
 				string=''
 		else:
 			if string.isdigit():
 				tokensList.append('int('+string+')')
-			tokensList.append(x)
+			tokensList.append(key)
 			string = ''
+
+	print(tokensList)
 
 	return tokensList
