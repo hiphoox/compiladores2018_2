41fe41afe07346a66ab02c721a57dc167652bf70
tanagromo <tanagromo@gmail.com>
2018-09-03 13:07:03 -0500

    fun statement

--- parser.py
+++ parser.py
@@ -1,17 +1,21 @@
 from tokenizer import tokenizer
 
 def expresion(tokens):
-    a = [item for item in tokens if item.isdigit()]
-    for i in a:
+    exp = [item for item in tokens if item.isdigit()]
+    for i in exp:
         tokens.remove(i)
-    return a
+    return exp
 
+def statement(tokens):
+    sta = [item for item in tokens if item.split("<")[0]=="Keyword" ]
+    return sta
+    
 
 def parser(tokens):
     ast=[]    
     ast.append(expresion(tokens))
+    ast.append(statement(tokens))
     print(tokens)
     return ast
-#def statement(tokens):
     
 print(parser(tokenizer('tarea1.c')))
