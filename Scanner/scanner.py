f88b5118ceba6603246803a4cb9ce2fd7409ae0e
Gerardo-Ramos <42423509+Gerardo-Ramos@users.noreply.github.com>
2018-09-02 21:22:12 -0500

    Analizador lexico

--- Scanner/scanner.py
+++ Scanner/scanner.py
@@ -1,26 +1,28 @@
+import re
 
-# coding: utf-8
-
-# In[ ]:
-
+asg={"int":"INT","rint":"INT", "main":"MAIN", "return":"RETURN", "(":"Opare", ")":"CPare", "{":"Obrace", "}":"Cbrace", ";":"SEMICOLON"}
 tkns=[]
+patron=re.compile('[(){};]')
+patron2=re.compile('\W+')
+patron3=re.compile('\d')
 def scanner():
     f=open("return_2.c", "r")
     for linea in f:
-        palabra=""
         linea=linea.strip(" ").rstrip(" ")
-        for j in linea:
-            if j == "(" or j == ")" or j == "{" or j== "}" or j==";" or j=="2":
-                if palabra != "":
-                    tkns.append(palabra)
-                tkns.append(j)
-                palabra=""
-            elif j != " ":
-                palabra+=j
-            else:
-                tkns.append(palabra)
-                palabra=""
+        palabra=patron.findall(linea)
+        for i in palabra:
+            if asg.has_key(i):
+                tkns.append(asg[i])
+        palabra=patron2.split(linea)
+        for i in palabra:
+            if asg.has_key(i):
+                tkns.append(asg[i])
+        palabra=patron3.findall(linea)
+        if len(palabra) != 0:
+            for i in palabra:
+                tkns.append(asg["rint"]+"<"+i+">")
     f.close()
+            
 scanner()
 print(tkns)
 
