80948f7f81acf654a06d8762b472584535a4f59b
Gerardo-Ramos <42423509+Gerardo-Ramos@users.noreply.github.com>
2018-09-06 17:55:14 -0500

    Analizador sintactico

---
+++ Scanner/codeGeneration.py
@@ -0,0 +1,30 @@
+'''Autor: Ramos Estrada Gerardo
+Descricion: Este programa genera el archivo ensamblador recibiendo como
+parametro un AST'''
+
+aux=[]
+from scanner import scanner
+from parser1 import parser
+
+def hijoDerecho(arbol):
+    return arbol[2]
+
+def hijoIzquierdo(arbol):
+    return arbol[1]
+    
+def postOrden(arbol):    
+    if len(arbol) != 0:
+        postOrden(hijoIzquierdo(arbol))
+        postOrden(hijoDerecho(arbol))
+        aux.append(arbol[0])
+        
+def generation(ast):
+    postOrden(ast)
+    archivo=open("retun_2.asm","w")
+    archivo.write(".globl _"+aux[2]+"\n")
+    archivo.write("_"+aux[2]+":"+"\n")
+    archivo.write("movl    $"+str(aux[0])+", %eax"+"\n")
+    archivo.write("ret")
+    archivo.close()
+    
+generation(parser(scanner()))
