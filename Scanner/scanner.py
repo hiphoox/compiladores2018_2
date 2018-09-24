7589ba356793da5b6534124a69b87b64f0514757
Gerardo-Ramos <42423509+Gerardo-Ramos@users.noreply.github.com>
2018-09-23 22:03:24 -0500

    Scanner

--- Scanner/scanner.py
+++ Scanner/scanner.py
@@ -1,21 +1,17 @@
 '''Autor: Ramos Estrada Gerardo
-Descripcion: Este programa realiza la función de escanear un programa 
+Descripcion: Este programa realiza la funcion de escanear un programa 
 en lenguaje C y obtener los tokens para mandarlos al parser'''
 import re
 
 def scanner():
-    tkns={"int":"INT","rint":"ID", "main":"MAIN", "return":"RETURN", "(":"Opare", ")":"CPare", "{":"Obrace", "}":"Cbrace", ";":"SEMICOLON"}
+    tkns={"int":"INTKEYWORD","rint":"INT", "main":"ID", "return":"RETURNKEYWORD", "(":"OPENPARE", ")":"CLOSEPARE", "{":"OPENBRACE", "}":"CLOSEBRACE", ";":"SEMICOLON"}
     patron=re.compile('[(){};]')
     patron2=re.compile('\W+')
-    patron3=re.compile('\d')
+    patron3=re.compile('\d+')
     tokens=[]
     archivo=open("return_2.c", "r")
     for linea in archivo:
         linea=linea.strip(" ")
-        palabra=patron.findall(linea)
-        for iterador in palabra:
-            if tkns.has_key(iterador):
-                tokens.append(tkns[iterador])
         palabra=patron2.split(linea)
         for iterador in palabra:
             if tkns.has_key(iterador):
@@ -23,7 +19,11 @@ def scanner():
         palabra=patron3.findall(linea)
         if len(palabra) != 0:
             for iterador in palabra:
-                tokens.append(tkns["rint"]+"<"+iterador+">")
+                tokens.append(int(iterador))
+        palabra=patron.findall(linea)
+        for iterador in palabra:
+            if tkns.has_key(iterador):
+                tokens.append(tkns[iterador])
     archivo.close()
     return tokens
 
