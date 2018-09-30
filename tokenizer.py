315a0368f0f45c7204def07c89262009700fad56
tanagromo <tanagromo@gmail.com>
2018-09-30 14:25:21 -0500

    add regex and fix label system

--- tokenizer.py
+++ tokenizer.py
@@ -1,15 +1,25 @@
+import re
+
 def tokenizer(file):
-	tokens = {'(':'OpenParen', ')': 'CloseParen', '{': 'OpenBrace', '}': 'CloseBrace', ';': 'Semicolon'}
+	symbols = {'(':'OpenParen', ')': 'CloseParen', '{': 'OpenBrace', '}': 'CloseBrace', ';': 'Semicolon'}
 	#las key words se pueden leer de un archivo
 	key={'auto','break','case','char','const','continue','default','do','double','else','enum','extern','float',
             'for','if','inline','int','long','register','restrict','return','sizeof','static','struct','switch',
             'typedef','union','unsigned','void','volatile','while'}
 	program = open(file, 'r').read().replace("\n", "").replace("\t", "")
-	for x in tokens:
-	    program = program.replace(x, ' '+tokens[x]+'<'+x+'>'+' ')
-        for y in key:
-            program = program.replace(y,' Keyword'+'<'+y+'> ')
-	return [item+'' for item in program.split(" ") if len(item) != 0]
+	for x in symbols:
+	    program = program.replace(x, ' '+symbols[x]+':'+x)
+	for y in key:
+		program = program.replace(y,' Keyword'+':'+y)	
+	tokens = [item+'' for item in program.split(" ") if len(item) != 0]
+	for i in tokens:
+		if ":" not in i:
+			digito=re.search(r'[0-9]+',i)
+			if digito:
+				tokens[tokens.index(i)]='INT:'+i
+			chain=re.search(r'[a-zA-Z]+',i)
+			if chain:
+				tokens[tokens.index(i)]='ID:'+i
+	return tokens
 
-tokenizer('tarea1.c')
-
+print(tokenizer('tarea1.c'))
