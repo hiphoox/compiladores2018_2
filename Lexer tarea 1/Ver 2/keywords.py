dddc1782eec69fe42eb999bd64ae7a470de18687
Ramsés Castro <42414696+Ramy34@users.noreply.github.com>
2018-10-01 18:44:52 -0500

    Update keywords.py

--- Lexer tarea 1/Ver 2/keywords.py
+++ Lexer tarea 1/Ver 2/keywords.py
@@ -1,14 +1,14 @@
-class keywords:
-	def __init__(self,elemento):
-		self.elemento = elemento
-		self.name = nombre(elemento)
-
-	def nombre(elemento):
+def nombre(elemento):
 		if(elemento == "int"):
 			return "INTKEYWORD"
 		elif(elemento == "return"):
 			return "RETURNKEYWORD"
 		elif(elemento == "main"):
-			return ID
+			return "ID"
 		else:
 			return "Error"
+
+class keywords:
+	def __init__(self,elemento):
+		self.elemento = elemento
+		self.name = nombre(elemento)
