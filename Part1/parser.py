0a3c5a0beef1a216c7fdba1a62b828cff5023698
Gerardo Arjona <gerardo.arj15@gmail.com>
2018-09-30 20:18:14 -0500

    Updated & Fix Parser, fixed missplaced return, updated parser to compare instances of c_classes
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

--- Part1/parser.py
+++ Part1/parser.py
@@ -1,5 +1,8 @@
 import re
 import os
+
+from c_classes import *
+
 from lex_function import lex
 
 class Node:
@@ -8,31 +11,38 @@ class Node:
         self.parent = parent
         self.son = son
 
-def expression(ret):
+def expression(tokens):
     num=[]
-    for l in ret:
-        numero_searched=re.search(r'INT<[0-9]+>',l)
-        if(numero_searched):
-            numero=re.search(r'[0-9]+',numero_searched.group(0))
-            if(numero):
-                num.append(numero.group(0))
-            return num
+    for t in tokens:
+        if  isinstance(t, Literal_num):
+            num.append(t)
+            break
+    return num
 
 def statement(tokens):
     stat=[]
     for t in tokens:
-        if(t=="RETURN"):
+        if  isinstance(t, Ret_kw):
             stat.append(t)
             tokens.remove(t)
-        else:
-            stat.append(expression(t))
+            stat.append(expression(tokens))
+        elif isinstance(t, Semicolon):
+            tokens.remove(t)
     return stat
 
 def program(tokens):
     p=[]
     for t in tokens:
-        if(t=="ID<main>"):
+        if isinstance(t, Id_kw):
             p.append(t)
+            tokens.remove(t)
+        elif isinstance(t, Open_paren):
+            tokens.remove(t)
+        elif isinstance(t, Close_paren):
+            tokens.remove(t)
+        elif isinstance(t, Open_brace):
+            tokens.remove(t)
+        elif isinstance(t, Close_brace):
             tokens.remove(t)
         else:
             p.append(statement(tokens))
@@ -42,18 +52,16 @@ def program(tokens):
 def parse(tokens):
     ast=[]
     for t in tokens:
-        if(t=="INT_ID<int>"):
+        if isinstance(t, Int_kw):
             ast.append(t)
             tokens.remove(t)
         else:
             ast.append(program(tokens))
             break
+    
+    print("----- AST -----")
     print(ast)
+    print()
     return ast
-
-
-
-
-
 
 parse(lex())
