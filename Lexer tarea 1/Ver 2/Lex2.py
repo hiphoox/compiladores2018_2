206c4bcc2a2073c2fabc0680f6ca7060c34b3f88
Ramsés Castro <42414696+Ramy34@users.noreply.github.com>
2018-10-01 18:44:30 -0500

    Update Lex2.py

--- Lexer tarea 1/Ver 2/Lex2.py
+++ Lexer tarea 1/Ver 2/Lex2.py
@@ -1,32 +1,34 @@
 import os, re, sys
-import os, re, sys, specialToken
+from specialToken import specialToken
+from keywords import keywords
 
-def separar(renglon,token_list):
+def separar(renglon,lista_token):
 	renglon = renglon.strip()
+	print(renglon + "\n")
 	id = re.match('\\(*[A-Za-z][A-Za-z0-9_]*\\)*', renglon)
 	numero = re.match('[0-9]',renglon)
-	print(numero)
 	if (len(renglon)==0):
 	    return
 	if(id):
-		token_list.append([keyWords([id.group(0)]),id.group(0)])
-		separar(renglon.lstrip(id.group(0)),token_list)
+		obj = keywords(id.group(0))
+		lista_token.append([obj.elemento, obj.name])
+		separar(renglon.lstrip(id.group(0)),lista_token)
 	elif(numero):
-		token_list.append(['Int',numero.group(0)])
-		separar(renglon.lstrip(numero.group(0)),token_list)
+		lista_token.append(['Int',numero.group(0)])
+		separar(renglon.lstrip(numero.group(0)),lista_token)
 	else:
-		tok = specialTokens(renglon[0])
-		if(tok.name() != "Error"):
-			token_list.append([tok.caracter,tok.name])
-			separar(renglon.lstrip(Token[tok].value),token_list)
+		tok = specialToken(str(renglon[0]))
+		if(tok.name != "Error"):
+			lista_token.append([tok.caracter,tok.name])
+			separar(renglon.lstrip(Token[tok].value),lista_token)
 				
 def leerArchivo(source_file):
     with open(source_file,'r') as file:
         renglon = file.read()
     return renglon
 			
-token_list = []
+lista_token = []
 source_file = sys.argv[1]
 renglon = leerArchivo(source_file)
-separar(renglon,token_list)
-print(token_list)
+separar(renglon,lista_token)
+print(lista_token)
