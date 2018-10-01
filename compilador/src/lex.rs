e1f62f57d49b36e329f8846355f1762020b985a0
mewbito <mewbito@gmail.com>
2018-10-01 16:23:33 -0500

    lexer funcionando completamente(sin comentarios aun)

--- compilador/src/lex.rs
+++ compilador/src/lex.rs
@@ -2,47 +2,45 @@ extern crate regex;
 use self::regex::Regex;
 
 
-pub enum Token{
-    OpenBrace,
-    CloseBrace,
-    OpenParen,
-    CloseParen,
-    SemiColon,
-    Identifier,
-    IntKeyword,
-    MainKeyword,
-    ReturnKeyword,
-    Int,
-
-}
-
-
-
-
-
-pub fn lex(programa: &str) -> Vec<&str>{
+pub fn lex( programa: &str) -> Vec<&str>{
     let tok: Vec<&str> = programa.split(' ').collect();
+    println!("{:?}",tok);
     let reid = Regex::new(r"[A-Za-z][A-Za-z0-9_]*").unwrap();
     let reint = Regex::new(r"[0-9]").unwrap();
-    let _rechar= Regex::new(r"[[:punct:]]").unwrap();
+    let rechar= Regex::new(r"[(){};]").unwrap();
     let mut tokf = vec!();
-    for x in tok{
+    for mut x in tok{
       if reid.is_match(x){
-          let cap =reid.captures(x).unwrap();
-          println!("{}",cap.get(0).map_or("", |m| m.as_str()));
-          match cap.get(0).map_or("", |m| m.as_str()) {
-            "int" => tokf.push("IntKeyword"),
-            "main" => tokf.push("MainKeyword"),
-            "return" => tokf.push("ReturnKeyword"),
-            _=> println!("id no encontrado"),
-          }
+            let cap =reid.captures(x).unwrap();
+            println!("{}",cap.get(0).map_or("", |m| m.as_str()));
+            match cap.get(0).map_or("", |m| m.as_str()) {
+                      "int" => tokf.push("IntKeyword"),
+                      "main" => tokf.push("MainKeyword"),
+                      "return" => tokf.push("ReturnKeyword"),
+                      _=> println!("id no encontrado"),
+            }
 
       }
       if reint.is_match(x){
-        let cap =reint.captures(x).unwrap();
-        println!("{}",cap.get(0).map_or("", |m| m.as_str()));
-        tokf.push(cap.get(0).map_or("", |m| m.as_str()));
+          let cap =reint.captures(x).unwrap();
+              println!("{}",cap.get(0).map_or("", |m| m.as_str()));
+              tokf.push(cap.get(0).map_or("", |m| m.as_str()));
       }
+      if rechar.is_match(x){
+      let rechar= Regex::new(r"[(){};]").unwrap();
+            for cap in rechar.captures_iter(x){
+              match cap.get(0).map_or("", |m| m.as_str()){
+                  "{" => tokf.push("OpenBrace"),
+                  "}"=> tokf.push("CloseBrace"),
+                  "("=> tokf.push("OpenParen"),
+                  ")"=> tokf.push("CloseParen"),
+                  ";"=> tokf.push("SemiColon"),
+                  _=> println!("error :("),
+              }
+              }
+
+              }
+
     }
     tokf
 }
--- compilador/src/main.rs
+++ compilador/src/main.rs
@@ -1,5 +1,8 @@
 extern crate compilador;
 use compilador::lex::*;
+extern crate regex;
+use self::regex::Regex;
+
 
 
 fn main(){
@@ -7,7 +10,10 @@ fn main(){
    let s="int main() {
     return 2;
 }";
-    let tokens: Vec<&str> =lex(s);
+    let re=Regex::new(r"[^0-9a-zA-Z(){}+; ]").unwrap();
+    let programa = re.replace_all(s,"");
+    let tokens: Vec<&str> =lex(&programa);
+
     println!("{:?}",tokens);
 
 
