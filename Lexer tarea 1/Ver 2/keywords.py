ce69c8ffab9c1a0ac57e6aad2b4cfc0707121aee
Ramsés Castro <42414696+Ramy34@users.noreply.github.com>
2018-10-01 18:05:07 -0500

    Add files via upload

---
+++ Lexer tarea 1/Ver 2/keywords.py
@@ -0,0 +1,14 @@
+class keywords:
+	def __init__(self,elemento):
+		self.elemento = elemento
+		self.name = nombre(elemento)
+
+	def nombre(elemento):
+		if(elemento == "int"):
+			return "INTKEYWORD"
+		elif(elemento == "return"):
+			return "RETURNKEYWORD"
+		elif(elemento == "main"):
+			return ID
+		else:
+			return "Error"
