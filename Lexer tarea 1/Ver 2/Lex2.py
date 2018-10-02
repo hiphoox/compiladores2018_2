fab033921aede6bad94e723346d1372df1de9277
Ramsés Castro <42414696+Ramy34@users.noreply.github.com>
2018-10-01 21:46:22 -0500

    Update Lex2.py

--- Lexer tarea 1/Ver 2/Lex2.py
+++ Lexer tarea 1/Ver 2/Lex2.py
@@ -1,8 +1,10 @@
 import re
 from specialToken import specialToken
 from keywords import keywords
+from number import number
 
 def lexer(renglon,lista_token):
+	list = []
 	renglon = renglon.strip()
 	while(len(renglon) != 0):
 		renglon = renglon.strip()
@@ -12,6 +14,7 @@ def lexer(renglon,lista_token):
 			tok = keywords(id.group(0))
 			if(tok.name != "Error"):
 				lista_token.append([tok.elemento, tok.name])
+				list.append(tok)
 				renglon = renglon.lstrip(tok.elemento)
 			else:
 				print("Elemento no válido: " + tok.elemento)
@@ -19,15 +22,24 @@ def lexer(renglon,lista_token):
 				return lista_token
 			renglon = renglon.lstrip(id.group(0)) #Quitamos el elemento que acabamos de etiquetar
 		elif(numero): #En caso de que no sea un id pero si un número, lo etiqueta, de momento solo tomamos en cuenta enteros
-			lista_token.append(['Int',numero.group(0)])
-			renglon = renglon.lstrip(numero.group(0))
+			tok = number(int(numero.group(0)))
+			if(tok.type != "Error"):
+				lista_token.append([tok.numero, tok.type])
+				list.append
+				renglon = renglon.lstrip(numero.group(0))
+			else:
+				print("Elemento no válido: " + str(tok.numero))
+				lista_token = ["Error"]
+				return lista_token				
 		else: #En caso de que no sea ninguno de los dos anteriores lo podemos catalogar como token especial, por lo que lo verifica
 			tok = specialToken(renglon[0])
 			if(tok.name != "Error"): 
 				lista_token.append([tok.caracter,tok.name])
+				list.append(tok)
 				renglon = renglon.lstrip(tok.caracter)
 			else:
 				print("Elemento no válido: " + tok.caracter)
 				lista_token = ["Error"]
 				return lista_token
-	return lista_token
+	print (lista_token)
+	return list
