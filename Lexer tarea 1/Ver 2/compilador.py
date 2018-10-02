774502d174ae4c9f0a922337b026742831373238
Ramsés Castro <42414696+Ramy34@users.noreply.github.com>
2018-10-02 01:21:52 -0500

    Update compilador.py

--- Lexer tarea 1/Ver 2/compilador.py
+++ Lexer tarea 1/Ver 2/compilador.py
@@ -1,9 +1,11 @@
 import os, sys
-from Lex import lexer
 from archivos import leerArchivo
+from Lex import lexer
+from parser2 import paser_program
 			
 lista_token = [] #Lista de tokens
+ast = [None,None,None]
 source_file = sys.argv[1] #Pasamos el argumento del archivo a compilar
 renglon = leerArchivo(source_file) #leemos el archivo y lo pasamos a un renglón
 lista_token = lexer(renglon,lista_token) #Ejecutamos el lexer
-print(lista_token) #Imprimimos la lista
+ast = paser_program(lista_token,ast) #Ejecutamos el parser
