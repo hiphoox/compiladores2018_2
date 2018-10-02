9020257e8a1ad24da780094cb8ed7e7b4049149b
Ramsés Castro <42414696+Ramy34@users.noreply.github.com>
2018-10-01 19:40:18 -0500

    Update archivos.py

--- Lexer tarea 1/Ver 2/archivos.py
+++ Lexer tarea 1/Ver 2/archivos.py
@@ -1,5 +1,5 @@
 import os, sys
-def leerArchivo(source_file):
+def leerArchivo(source_file): #De momento solo abrimos un archivo y ponemos su contenido en una sola variable
 	with open(source_file,'r') as file:
 		renglon = file.read()
 	return renglon
