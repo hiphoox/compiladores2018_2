7a41746317efad52eaed1f81eaaa30214c13bb1c
Ramsés Castro <42414696+Ramy34@users.noreply.github.com>
2018-10-01 19:29:21 -0500

    Update Lex2.py

--- Lexer tarea 1/Ver 2/Lex2.py
+++ Lexer tarea 1/Ver 2/Lex2.py
@@ -1,34 +1,33 @@
-import os, re, sys
+import re
 from specialToken import specialToken
 from keywords import keywords
 
-def separar(renglon,lista_token):
+def lexer(renglon,lista_token):
 	renglon = renglon.strip()
-	print(renglon + "\n")
-	id = re.match('\\(*[A-Za-z][A-Za-z0-9_]*\\)*', renglon)
-	numero = re.match('[0-9]',renglon)
-	if (len(renglon)==0):
-	    return
-	if(id):
-		obj = keywords(id.group(0))
-		lista_token.append([obj.elemento, obj.name])
-		separar(renglon.lstrip(id.group(0)),lista_token)
-	elif(numero):
-		lista_token.append(['Int',numero.group(0)])
-		separar(renglon.lstrip(numero.group(0)),lista_token)
-	else:
-		tok = specialToken(str(renglon[0]))
-		if(tok.name != "Error"):
-			lista_token.append([tok.caracter,tok.name])
-			separar(renglon.lstrip(Token[tok].value),lista_token)
-				
-def leerArchivo(source_file):
-    with open(source_file,'r') as file:
-        renglon = file.read()
-    return renglon
-			
-lista_token = []
-source_file = sys.argv[1]
-renglon = leerArchivo(source_file)
-separar(renglon,lista_token)
-print(lista_token)
+	while(len(renglon) != 0):
+		renglon = renglon.strip()
+		id = re.match('\\(*[A-Za-z][A-Za-z0-9_]*\\)*', renglon) #Revisamos el elemento que vamos a etiquetar
+		numero = re.match('[0-9]',renglon) #Revisamos si no es un número
+		if(id): #Si es un elemento id/keyword creará un objeto keyword y lo etiqueta
+			tok = keywords(id.group(0))
+			if(tok.name != "Error"):
+				lista_token.append([tok.elemento, tok.name])
+				renglon = renglon.lstrip(tok.elemento)
+			else:
+				print("Elemento no válido: " + tok.elemento)
+				lista_token = ["Error"]
+				return lista_token
+			renglon = renglon.lstrip(id.group(0)) #Quitamos el elemento que acabamos de etiquetar
+		elif(numero): #En caso de que no sea un id pero si un número, lo etiqueta, de momento solo tomamos en cuenta enteros
+			lista_token.append(['Int',numero.group(0)])
+			renglon = renglon.lstrip(numero.group(0))
+		else: #En caso de que no sea ninguno de los dos anteriores lo podemos catalogar como token especial, por lo que lo verifica
+			tok = specialToken(renglon[0])
+			if(tok.name != "Error"): 
+				lista_token.append([tok.caracter,tok.name])
+				renglon = renglon.lstrip(tok.caracter)
+			else:
+				print("Elemento no válido: " + tok.caracter)
+				lista_token = ["Error"]
+				return lista_token
+	return lista_token
