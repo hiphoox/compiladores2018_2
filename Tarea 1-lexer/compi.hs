199fe72cfe846f9e8358a0db46a400adf0ea08cf
RaulGalvanBazan <42521723+RaulGalvanBazan@users.noreply.github.com>
2018-10-16 11:39:11 -0500

    Update compi.hs
    Se agregó soporte para algunas expresiones con operadores unarios

--- Tarea 1-lexer/compi.hs
+++ Tarea 1-lexer/compi.hs
@@ -1,5 +1,6 @@
 import Data.Char
-import System.Environment   
+import System.Environment
+import System.Process
 
 
 data UnaryOp = Plus | Minus | Neg
@@ -21,8 +22,8 @@ data Token = UniOp UnaryOp
 operator :: Char -> UnaryOp
 operator c | c == '+' = Plus
            | c == '-' = Minus
-           | c == '~' = Neg
- 
+           | c == '~' = Neg 
+
 tokenize :: String -> [Token]
 tokenize [] = []
 tokenize (c : cs) 
@@ -47,15 +48,16 @@ is_Id_Kw c cs = let (name, cs') = span isAlphaNum cs in
 entero :: Char -> String -> [Token]
 entero c cs = 
    let (digs, cs') = span isDigit cs in
-   Entero (read (c : digs)) : tokenize cs'
-
+      Entero (read (c : digs)) : tokenize cs'
 
 --AST
-data Const= Const Int
+data UnOp=Min|Pls|Cmp
+      deriving (Show, Eq)
+data Exp= Const Int|Unary UnOp Exp
       deriving (Show, Eq)
 data Tipo = IntType
       deriving (Show, Eq)
-data Statement = ReturnVal Const
+data Statement = ReturnVal Exp
       deriving (Show, Eq)
 data Identifier = ID String
       deriving (Show, Eq)
@@ -68,43 +70,50 @@ data FunDec = Funcion (Tipo, Identifier, FunBod)
 data Prog = Program FunDec
       deriving (Show, Eq)
 
-
 --Parser
 
 lookAhead :: [Token] -> Token
 lookAhead [] = TokEnd
 lookAhead (t:ts) = t
 
-accept :: [Token] -> [Token]
-accept [] = error "Error, se espera un token"
-accept (t:ts) = ts
+restoLista :: [Token] -> [Token]
+restoLista [] = error "Error, se espera un token"
+restoLista (t:ts) = ts
 
 parse_funParam :: [Token] -> ([FunParam], [Token])
 parse_funParam (c:cs)=
    case c of
       RParen -> ([], cs)
-      _ -> error "Error durante el parseo: "
+      _ -> error "Error durante el parseo, se esperaba )"
 
-parse_exp :: [Token] -> (Const, [Token])
-parse_exp (c:cs)=
+parse_num :: Token -> Exp
+parse_num (Entero x) = Const x
+
+parse_exp :: [Token] -> (Exp, [Token])
+parse_exp tokens=
+   let (c, cs) = (lookAhead tokens, restoLista tokens) in
    case c of
-      (Entero x) -> (Const x, cs)
+      (Entero x) -> (parse_num (Entero x), cs)
+      UniOp Plus -> let (exp, rest) = parse_exp cs in (Unary Pls exp, rest)
+      UniOp Minus -> let (exp, rest) = parse_exp cs in (Unary Min exp, rest)
+      UniOp Neg -> let (exp, rest) = parse_exp cs in (Unary Cmp exp, rest)
       _ -> error "Token no válido o inexistente en parse_exp"
 
 parse_statements :: [Token] -> (Statement, [Token])
 parse_statements (c:cs)=
    case c of
-      ReturnKw -> let (exp, cs') = parse_exp cs in
+      ReturnKw -> let (exp, cs') = (parse_exp cs) in
                      case lookAhead cs' of
-                        Semicolon-> (ReturnVal exp, accept cs')
+                        Semicolon-> (ReturnVal exp, restoLista cs')
+                        _-> error "Se esperaba ;"
       _ -> error "Se esperaba devolver una expresion"
 
 parse_funBody :: [Token] -> FunBod
 parse_funBody tokens =
    let (statements, rest)= parse_statements tokens in
-      let (c, cs) = (lookAhead rest, accept rest) in
+      let (c, cs) = (lookAhead rest, restoLista rest) in
          case c of
-            RBrac-> let (c', cs') = (lookAhead cs, accept cs) in
+            RBrac-> let (c', cs') = (lookAhead cs, restoLista cs) in
                         case c' of
                            TokEnd-> Body statements
                            _->error "Error " 
@@ -112,25 +121,26 @@ parse_funBody tokens =
 
 makeBody :: [Token] -> FunBod
 makeBody tokens =
-           let(c, cs) = (lookAhead tokens, accept tokens) in
+           let(c, cs) = (lookAhead tokens, restoLista tokens) in
             case c of
                LBrac -> parse_funBody cs
                _ -> error "Se esperaba {"
 
 parse_fun :: [Token] -> FunDec
 parse_fun tokens=
-   let (funType, cs) = (lookAhead tokens, accept tokens) in
+   let (funType, cs) = (lookAhead tokens, restoLista tokens) in
       case funType of
-         IntKw -> let (fun_name, cs') = (lookAhead cs, accept cs) in
+         IntKw -> let (fun_name, cs') = (lookAhead cs, restoLista cs) in
                      case fun_name of
-                        (Ident x) -> let (isParen, cs'') =  (lookAhead cs', accept cs') in
+                        (Ident x) -> let (isParen, cs'') =  (lookAhead cs', restoLista cs') in
                                        case isParen of
                                           LParen -> let (params, cs''') = parse_funParam cs'' in
                                              case params of
                                                 []->Funcion (IntType, ID x, makeBody cs''')
-                                                _-> error "Se esperaba )"
-                        _-> error "Se esperaba un identificador"
-         _-> error "Se esperaba tipo de la funcion"
+                                                _-> error "Error de parseo, argumentos no válidos"
+                        _-> error "Error de parseo, Identificador no válido"
+         _-> error "Error de parseo, tipo de la función no válido"
+
 
 
 parser :: [Token] -> Prog
@@ -143,11 +153,15 @@ generator program=
    let main = ".globl main\n" in
       case program of
          (Program x) -> case x of
-                           Funcion(IntType, (ID y), Body z)-> let funId= id y in
-                                                               case z of
-                                                                  ReturnVal (Const i) -> let fun = "_"++show y ++ ":\n " in
-                                                                     main ++  funId ++ ":\nmovl $" ++ show i ++ ", %eax \nret"
-     
+            Funcion(IntType, (ID y), Body z)-> let (genH, genT) = (main ++ id y ++ ":\nmovl $", ", %eax \nret\n") in
+               case z of
+                  ReturnVal (Const i) -> genH ++ show i ++ genT
+                  ReturnVal (Unary Pls (Const i)) -> genH ++ show i ++ genT
+                  ReturnVal (Unary Min (Const i)) -> genH ++"-"++show i ++ genT
+                  ReturnVal (Unary Cmp (Const i)) -> genH ++"~"++show i ++ genT
+                  _->error "Error en la generacion de codigo de return"
+
 main = do args <- getArgs
           file <- readFile (args!!0)
-          writeFile "out.s" (generator (parser (tokenize file)))
+          writeFile "cfile.s" (generator (parser (tokenize file)))
+          createProcess(proc "gcc" ["cfile.s", "-o", "exe"])
