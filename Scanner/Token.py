f9f9456922bb4d189a1776893a8e69cccb4d0214
Gerardo-Ramos <42423509+Gerardo-Ramos@users.noreply.github.com>
2018-12-07 19:23:42 -0600

    Token.py

---
+++ Scanner/Token.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class tokenLista(Enum):
+    OpenBrace = "OpenBrace"
+    CloseBrace = "CloseBrace"
+    OpenPare = "OpenPare"
+    ClosePare = "ClosePare"
+    Semicolon = "Semicolon"
+    ReturnKeyword = "ReturnKeyword"
+    IntKeyword = "Int"
+    Id = "ID: "
+    Constant = "Constant: "
+    Negation = "Negation"
+    BitwiseComplement = "BitwiseComplement"
+    LogicalNegation = "LogicalNegation"
+    Addition = "Addition"
+    Multiplication = "Multiplication"
+    Division = "Division" 
