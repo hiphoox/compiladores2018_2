8e9ae9d75bc7987a13493141604c963a861c63b9
berenice <bemesa21@gmail.com>
2018-12-12 04:38:28 -0600

    se cambia manera de mostrar errores y termina ejecucion en caso de error

--- Python/compiler/main.py
+++ Python/compiler/main.py
@@ -2,6 +2,7 @@ import sys
 from tokens import Token
 from lexer import *
 from parser import *
+from generator import *
 from colorama import init, Fore, Back, Style
 
 #leyendo de un archivo
@@ -13,13 +14,25 @@ print(Fore.BLUE + Back.WHITE + Style.DIM+'\n\nProbando con ' + sys.argv[1])
 
 
 tokens = tokeniza(linea)
-print(Fore.BLUE + Back.CYAN + Style.DIM +"\------------------------------Tokens------------------------------------")
-print(tokens)
-print('\-------------------------------------------------------------------------' + Fore.RESET)
 
-if(type(tokens) is list):
+if(tokens[0] !=  False):
+	print(Fore.BLUE + Back.CYAN + Style.DIM +"\------------------------------Tokens------------------------------------")
+	print(tokens)
+	print('\-------------------------------------------------------------------------' + Fore.RESET)
 	ast = parse_program(tokens)	
-if(ast != None):
-	print(Fore.MAGENTA + Back.WHITE + Style.DIM +"\------------------------------------AST--------------------------------")
-	imprime(ast)
-	print("\-----------------------------------------------------------------------")
+	if(ast[0] != False):
+		print(Fore.MAGENTA + Back.WHITE + Style.DIM +"\------------------------------------AST--------------------------------")
+		imprime(ast)
+		print(genera(ast[0][1]))
+		print("\-----------------------------------------------------------------------")
+	else:
+		print(Fore.RED + Back.WHITE + Style.DIM+'\----------------------------------------¡Error!------------------------------------/')
+		print('Unexpected token ' + ast[1] + '\t' + 'Se esperaba ' + ast[2])
+		print('\-----------------------------------------------------------------------------------/')
+else:
+	print(Fore.RED + Back.WHITE + Style.DIM+'\----------------------------------------¡Error!------------------------------------/')
+	print('Invalid token: ' + tokens[1])
+	print('\-----------------------------------------------------------------------------------/')
+
+
+
