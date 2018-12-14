fc74ffe2dc73ad566d0911c0ad8be93203cfd479
Rodrigo Aboites <40849129+Aboites@users.noreply.github.com>
2018-12-13 23:53:28 -0600

    Update genCodigo.py
    Validación del AST con las mismas reglas en las operaciones que se mencionaron anteriormente (dos asociaciones no funcionan y falta implementar las operaciones diferentes a la suma) y unión con el programa principal "Prueba.py"

--- Compilador/genCodigo.py
+++ Compilador/genCodigo.py
@@ -1,5 +1,3 @@
-import os
-
 def listaRetorno (lista, bandera):
     #print (lista)
     e1, e2 , ex, instruccion = '', '', '', ''
@@ -7,7 +5,7 @@ def listaRetorno (lista, bandera):
         if (str(type(i)).find("list") != -1):
             if (bandera == 1):
                 ex, bandera = listaRetorno(i, 0)
-                e1 = e1[:e1.find("\n\tpush")-1]
+                e1 = e1[:e1.find("\n\tpush")]
                 #print (e1)
                 ex = ex + "\n\tpush %eax\n\tpop %ecx" + e1 + "\n\taddl %ecx, %eax"
                 bandera = 0
@@ -27,50 +25,68 @@ def listaRetorno (lista, bandera):
                 e2 = "\n\tpop %ecx\n\tmovl  $" + i[4:-1] + ", %eax\n\taddl %ecx, %eax"
                 bandera = 0
                 instruccion = instruccion + e2
+            elif (i in ['+', '-', '*', '/', '~', '!', ]):
+                pass
             else:
+                print("INVALIDO")
+                instruccion = "INVALIDO"
                 break
+        else:
+            print ("INVALIDO")
+            instruccion = "INVALIDO"
+            break
         #instruccion = ex + e1 + e2
         #print (instruccion)
     return instruccion, bandera
 
-def valorRetorno(AST):
+def generadorASM(AST):
     #print (AST[0])
     #print (type(AST[0]))
     valRet = AST.pop(0)
     tipoRecibido = type(valRet)
     if (str(tipoRecibido).find("str") != -1):
         instruccionVRetorno = "\n\tmovl  $" + valRet[4:-1] + ", %eax"
-        print (instruccionVRetorno)
+        #print (instruccionVRetorno)
     elif (str(tipoRecibido).find("list") != -1):
-        print ("LISTA DE OPERACIONES")
+        #print ("LISTA DE OPERACIONES")
         instruccionVRetorno, bandera = listaRetorno(valRet, 0)
-        print (instruccionVRetorno)
+        #print (instruccionVRetorno)
     else:
-        print ("AST INVALIDO")
-    #print (AST)
-    return "blah   "
+        instruccionVRetorno = "AST INVALIDO"
+        return instruccionVRetorno
+    
+    if (instruccionVRetorno != 'INVALIDO'):
+        #print ("RETURN VERIFICATION")
+        if (AST.pop(0) == 'RETURN KEYWORD'):
+            instruccionVRetorno = instruccionVRetorno + "\n\tret"
+        else:
+            instruccionVRetorno = "AST INCORRECTO"
+            return instruccionVRetorno
+    else:
+        instruccionVRetorno = "AST INCORRECTO"
+        return instruccionVRetorno
+    
+    if (len(AST) != 0):
+        name = AST.pop(0)
+        functionName = name[3:-1]
+        instruccionVRetorno = "\t.globl _" + functionName + "\n_" + functionName + ":" + instruccionVRetorno
+        if (len(AST) != 0):
+            instruccionVRetorno = "AST INCORRECTO"
+            return instruccionVRetorno
+    else:
+        instruccionVRetorno = "AST INCORRECTO"
+        return instruccionVRetorno
+
+    return instruccionVRetorno
+
+    
+
 
 def x(programa):
     if (len(programa) == 3):
-        resultado = valorRetorno(programa)
-        print (resultado)
+        asm = generadorASM(programa)
+        #print (asm)
     else:
         print("Programa no compilado")
-        return 
-    return
-
-
-
-
-
-
-prueba1 = ['INT<234>', 'RETURN KEYWORD', 'MAIN ID']
-prueba2 = [['INT<1>', 'INT<2>', '+'], 'RETURN KEYWORD', 'MAIN ID']
-prueba3 = [['INT<4>', ['INT<5>', 'INT<6>', '*'], '+'], 'RETURN KEYWORD', 'MAIN ID']
-prueba4 = [[['INT<7>', 'INT<8>', '+'], 'INT<9>', '*'], 'RETURN KEYWORD', 'MAIN ID']
-prueba5 = [[[['INT<1>', 'INT<2>', '+'], 'INT<3>', '*'], 'INT<4>', '-'], 'RETURN KEYWORD', 'MAIN ID']
-x(prueba1)
-x(prueba2)
-x(prueba3)
-x(prueba4)
-x(prueba5)
+        return ("AST INVALIDO")
+    return asm
