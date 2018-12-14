40ea387743c2ea7ad26c67e3c2edb491d9b0145f
Rodrigo Aboites <40849129+Aboites@users.noreply.github.com>
2018-12-13 23:58:10 -0600

    Update Prueba.py
    Implementación del generador de código y una mejor impresión para cada prueba. 
    FALTANTE: La compilación del programa ensamblador ya generado ya que anteriormente tuve problemas para usar comandos de terminal en python.

--- Compilador/Prueba.py
+++ Compilador/Prueba.py
@@ -1,33 +1,38 @@
 from pruebaPar import parser
 from tokenizerV2 import tokenizerV2
+from genCodigo import x
 
 pruebaV = ['multi_digit.c', 'newlines.c', 'no_newlines.c', 'return0.c', 'return2.c', 'spaces.c']
 pruebaI = ['missing_paren.c', 'missing_retval.c', 'no_semicolon.c', 'no_space.c', 'wrong_case.c']
 
 print ("\t----------- Pruebas validas -----------")
 for i in pruebaV:
-    print ('\n'+i)
-    tokens = tokenizerV2(i)
-    print(tokens)
-    ast, programa = parser(tokens)
-    
-    if (len(programa) != 0):
-    	print(programa)
-    	#x(programa)
-        print "Compilacion realizada exitosamente"
-    else:
-    	print ("Compilacion no exitosa")
-
-print ("\t----------- Pruebas invalidas -----------")
-for i in pruebaI:
+    print ("------------- DIVISION ENTRE PRUEBAS -------------")
     print ('\n'+i)
     tokens = tokenizerV2(i)
     print(tokens)
     ast,programa = parser(tokens)
     
     if (len(programa) != 0):
-    	print(programa)
-    	#x(programa)
-        print "Compilacion realizada exitosamente"
+        print(programa)
+        asm = x(programa)
+        print ("Compilacion realizada exitosamente\n\n SU CODIGO ENSAMBLADOR QUEDA DE LA SIGUIENTE FORMA: \n\n")
+        print(asm)
     else:
-    	print ("Compilacion no exitosa")
+        print ("Compilacion no exitosa")
+
+print ("\t----------- Pruebas invalidas -----------")
+for i in pruebaI:
+    print ("------------- DIVISION ENTRE PRUEBAS -------------")
+    print ('\n'+i)
+    tokens = tokenizerV2(i)
+    print(tokens)
+    ast,programa = parser(tokens)
+    
+    if (len(programa) != 0):
+        print(programa)
+        asm = x(programa)
+        print ("Compilacion realizada exitosamente\n\n SU CODIGO ENSAMBLADOR QUEDA DE LA SIGUIENTE FORMA: \n\n")
+        print(asm)
+    else:
+        print ("Compilacion no exitosa")
