8b60499f3ff695e256e869a45ab6afbbbb37da83
Rodrigo Aboites <40849129+Aboites@users.noreply.github.com>
2018-12-13 22:54:08 -0600

    Update genCodigo.py
    El generador de código funciona, por ahora, sólo creando las instrucciones para el valor de retorno y para cuando se tiene una sola asociación (por ejemplo (1+2)*(3+4) no lo logra crear correctamente pero (1+2)*3+4 si lo hace). Falta también el cambiar las instrucciones correspondientes a las diferentes operaciones, por ahora todo lo toma como suma.

--- Compilador/genCodigo.py
+++ Compilador/genCodigo.py
@@ -1,27 +1,76 @@
 import os
 
-def x(programa):
-    if (len(programa) != 0):                    # Si el ast recibido no esta vacio siginifica que el parser logro revisar correctamente la sintaxis 
-        cadInicio = '.globl _main\n_main:\n'    # Estructura inicial del codigo ensamblador
-        for i in programa:                      # Recorremos nuestra lista de funciones en el programa
-            for j in i:                         # Verificamos los elementos encontrados en cada funcion
-                if "INT" in j:                  # En cuanto encontremos el valor de retorno asignamos ese valor en una variable auxiliar
-                    valorRet = j[4:-1]
-                    break;
-        
-        cadInicio = cadInicio + "\tmovl $" + str(valorRet) + ", %eax\n\tret\n" # Completamos la estructura del ensamblador con el valor de retorno encontrado
-        
-        ar = open("assembly.s", 'w+')       # Abrimos el archivo donde escribiremos nuestro codigo ensamblador
-        ar.write(cadInicio)                 # Lo escribimos en el archivo
-        
-        ar.close()                          # Cerramos el archivo
+def listaRetorno (lista, bandera):
+    #print (lista)
+    e1, e2 , ex, instruccion = '', '', '', ''
+    for i in lista:
+        if (str(type(i)).find("list") != -1):
+            if (bandera == 1):
+                ex, bandera = listaRetorno(i, 0)
+                e1 = e1[:e1.find("\n\tpush")-1]
+                #print (e1)
+                ex = ex + "\n\tpush %eax\n\tpop %ecx" + e1 + "\n\taddl %ecx, %eax"
+                bandera = 0
+                instruccion = instruccion + ex
+            else:
+                ex, bandera = listaRetorno(i, bandera)
+                ex = ex + "\n\tpush %eax"
+                bandera = 1
+                instruccion = ex
+            #print ("PRUEBA 2")
+        elif (str(type(i)).find("str") != -1):
+            if (i.find("INT") == 0 and bandera == 0):
+                e1 = "\n\tmovl  $" + i[4:-1] + ", %eax\n\tpush %eax"
+                bandera = 1
+                instruccion = instruccion + e1
+            elif (i.find("INT") == 0 and bandera == 1):
+                e2 = "\n\tpop %ecx\n\tmovl  $" + i[4:-1] + ", %eax\n\taddl %ecx, %eax"
+                bandera = 0
+                instruccion = instruccion + e2
+            else:
+                break
+        #instruccion = ex + e1 + e2
+        #print (instruccion)
+    return instruccion, bandera
 
-        os.system("as assembly.s -o out")   # Lo mandamos a compilar con el comando correspondiente
-
-        os.remove("assembly.s")             # Ya compilado borramos el archivo 'assembly.s'
-        print("COMPILACION EXITOSA") 
-
+def valorRetorno(AST):
+    #print (AST[0])
+    #print (type(AST[0]))
+    valRet = AST.pop(0)
+    tipoRecibido = type(valRet)
+    if (str(tipoRecibido).find("str") != -1):
+        instruccionVRetorno = "\n\tmovl  $" + valRet[4:-1] + ", %eax"
+        print (instruccionVRetorno)
+    elif (str(tipoRecibido).find("list") != -1):
+        print ("LISTA DE OPERACIONES")
+        instruccionVRetorno, bandera = listaRetorno(valRet, 0)
+        print (instruccionVRetorno)
     else:
-        print("COMPILACION NO EXITOSA")     # Si el ast esta vacio significa que hubo un error en sintaxis por lo que no se compila nada
+        print ("AST INVALIDO")
+    #print (AST)
+    return "blah   "
 
+def x(programa):
+    if (len(programa) == 3):
+        resultado = valorRetorno(programa)
+        print (resultado)
+    else:
+        print("Programa no compilado")
+        return 
     return
+
+
+
+
+
+
+prueba1 = ['INT<234>', 'RETURN KEYWORD', 'MAIN ID']
+prueba2 = [['INT<1>', 'INT<2>', '+'], 'RETURN KEYWORD', 'MAIN ID']
+prueba3 = [['INT<4>', ['INT<5>', 'INT<6>', '*'], '+'], 'RETURN KEYWORD', 'MAIN ID']
+prueba4 = [[['INT<7>', 'INT<8>', '+'], 'INT<9>', '*'], 'RETURN KEYWORD', 'MAIN ID']
+prueba5 = [[[['INT<1>', 'INT<2>', '+'], 'INT<3>', '*'], 'INT<4>', '-'], 'RETURN KEYWORD', 'MAIN ID']
+x(prueba1)
+x(prueba2)
+x(prueba3)
+x(prueba4)
+x(prueba5)
