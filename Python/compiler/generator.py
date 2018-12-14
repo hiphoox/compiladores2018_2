24fc9b958111b9d39f974bcf1775cbd5f75bdbb2
berenice <bemesa21@gmail.com>
2018-12-13 23:28:55 -0600

    generador recorre correctamente arbol para biOp

--- Python/compiler/generator.py
+++ Python/compiler/generator.py
@@ -1,26 +1,21 @@
 def genera(ast,nodos = []):
 	if(type(ast)!=bool):
-		if(type(ast[0]) != list):
-			print(len(ast))
-			#print(ast[len(ast)-1])
-			if(ast[0] == 'return'):
-				nodos.append(ast[0])
-				print('yey')
-			#else:
-				#ast.append(True)
-		if(type(ast[1]) != list):
-			#print('equis')
-			print(ast[1])
-			nodos.append(ast[1]) ##llegando a los terminales
-			return
-		else:
-			if(len(ast)==2):  #donde se recursea
-				#ast.append(True)
-				print(ast)
-				genera(ast[1])
-			elif(len(ast)==3):
-				#ast.append(True)
-				genera(ast[1])#donde se recursea
-				nodos.append(ast[0][1])
-				genera(ast[2])
+		if(len(ast)==2 and type(ast[1]) != list):  #hoja de ast
+			cons = ast[1] 
+			return cons
+		elif(len(ast)==2):  #nodo solo con un hijo
+			expr = genera(ast[1])
+			if(ast[0] == 'return' and type(expr) != list):
+				return  'return' + expr 
+			return expr
+		elif(len(ast)==3): #nodo con dos hijos
+			term1 = ast[1] if type(ast[1]) != list else genera(ast[1])	
+			op = ast[0][1]
+			term2 = ast[2] if type(ast[2]) != list else genera(ast[2])
+			return this_is_my_op(term1,op,term2)
 		return nodos
+
+
+def this_is_my_op(op1,sign,op3):
+	#print(str(op1) + sign + str(op3))
+	return '('+str(op1) + sign + str(op3)+')'
