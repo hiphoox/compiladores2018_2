7ea1ddde601cb0d943378b70ff961e37ab471b92
bryantserrano96 <42443238+bryantserrano96@users.noreply.github.com>
2018-12-14 13:19:05 -0600

    Update compiler.py

--- compiler.py
+++ compiler.py
@@ -1,53 +1,43 @@
-import lexer
-import parser
-import generate
+import lexer, parser, generate
 
-#import os to write in terminal
 import os
 
-#to read document from terminal
 from sys import*
 
-#function to open file
 def open_file( file ):
-	data = open( file,"r" ).read()
+	data = open( file, "r" ).read()
 	return data
 
-#stores data from second argument in terminal
-data = open_file( argv[1] ).strip()
+data = open_file( argv[1] )
 
-#x for lexer
-x1 = lexer.Lexer( data )
-x = x1.lex( data )
+l = []
+lexer.lex(data,l)
 
-#y for parser
-y = parser.parse_program( x )
+p = []
+parser.parseProg(l,p)
 
-#z for generate
-z = generate.write_asm( y )
+g = []
+generate.gen(p,g)ec
 
-#condition if there's a flag 
 if len( argv ) == 3:
-	#-l for lexer (list of tokens)
 	if argv[2] == "-l":
-		print x
+		print( l )
 		exit()
-	#-p for parser (AST)
+
 	if argv[2] == "-p":
-		parser.printT(y)
+		print( p )
 		exit()
-	#-g for generate (.s code)
+
 	if argv[2] == "-g":
-		print z
+		for item in g:
+			print item
 		exit()
 
-#write assembly code
 f = open( "assembly.s","w" )
-f.write(z)
-f.close()
+for item in g:
+	f.write( item )
+f.close() 
 
-#creates assembly executabe
-os.system("gcc -m64 assembly.s -o out")
+os.system( "gcc -m64 assembly.s -o out" )		
 
-#erase assembly file
-os.system("rm assembly.s")
+os.system( "rm assembly.s" )
