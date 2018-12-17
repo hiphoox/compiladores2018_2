74415fd3c7f8e64d261c1091f6292da57e4262bf
bryantserrano96 <42443238+bryantserrano96@users.noreply.github.com>
2018-12-17 07:50:41 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,43 +1,70 @@
+#calls modules from anytree to reed tree from parser
+from anytree import Node, RenderTree, PreOrderIter
+
 g = []
 	
-def writeGlobl( ast, asmProg = g ):
+def writeGlobl( ast, asmProg ):
     if ast[0] == "Program":
-        asmProg.append("\t.globl main\n")
-        return asmProg
+        asmProg.append( "\t.globl main\n" )
+        return asmProg, ast[1:]
  
-def writeMain( ast, asmProg = g ):
-    if ast[0] == "main":
+def writeFun( ast, asmProg ):
+    if ast[0] == "Function":
         asmProg.append( "main:\n")
-        return asmProg
+        return asmProg, ast[1:]
         
-def writeRet( ast, asmProg = g):
+def writeRet( ast, asmProg ):
     if ast[0] == "Return":
-        m = writeMov(ast[1:], asmProg)
-        asmProg.append("\tret\n")
+    	if "+" in ast: 
+    		asmProg, rest = writeSum( ast[2:], asmProg )
+    	if "*" in ast:
+    		asmProg, rest = writeMult( ast[2:], asmProg )
+    	if isinstance( ast[1], int ): 
+    		asmProg, rest = writeMov( ast[1:], asmProg )
+        if "-" in ast or "!" in ast: 
+        	asmProg = writeNeg( ast[2], asmProg )
+        asmProg.append( "\tret\n" )
         return asmProg
 
-def writeNeg( ast, asmProg = g ):
-	if ast[0] == "-":
-		asmProg.append("neg\t%eax\n")
-		return asmProg
+def writeMov( ast, asmProg ):
+    asmProg.append( "\tmovl\t$"+str(ast[0])+", %eax\n" ) 
+    return asmProg, ast[1:]
 
-def writeMov( ast, asmProg = g ):
-    if ( isinstance(ast[0], int) ):
-        asmProg.append("\tmovl\t$"+str(ast[0])+", %eax\n") 
-        return asmProg
+def writeNeg( ast, asmProg ):
+	asmProg.append( "\tneg\t%eax\n" )
+	return asmProg
 
-def gen( ast, asm = g ):
-    if ast:
-        if writeGlobl(ast, asm):
-            gen( ast[1:], asm  )
-        
-        if writeMain(ast, asm):
-            gen( ast[1:], asm )
-        
-        if writeRet(ast, asm):
-            gen( ast[1:],asm)
+def writeSum( ast, asmProg ):
+	
+	asmProg, rest = writeMov( ast, asmProg )
+	asmProg.append( "\tpush \t%eax\n" )
+	asmProg, rest = writeMov( rest, asmProg )
+	asmProg.append( "\tpop \t%ecx\n" )
+	asmProg.append( "\taddl \t%ecx, %eax\n" )
+	
+	return asmProg, "None"
 
-        if writeNeg( ast, asm ):
-        	gen( ast[1:],asm )
-    else:
-        return asm
+def writeMult( ast, asmProg ):
+	
+	asmProg, rest = writeMov( ast, asmProg )
+	asmProg.append( "\tpush \t%eax\n" )
+	asmProg, rest = writeMov( rest, asmProg )
+	asmProg.append( "\tpop \t%ecx\n" )
+	asmProg.append( "\timul \t%ecx, %eax\n" )
+	
+	return asmProg, "None"
+
+def writeGen( ast, asmProg = g ):
+	if ast:
+		if writeGlobl( ast,asmProg ): writeGen( ast[1:],asmProg )
+		if writeFun( ast, asmProg ): writeGen( ast[1:],asmProg )
+		if writeRet( ast, asmProg ): writeGen( ast[1:],asmProg )
+	return asmProg 
+
+
+def gen( ast ):
+	t = [node.name for node in PreOrderIter( ast )]
+	
+	g = writeGen(t)
+	
+	return g
