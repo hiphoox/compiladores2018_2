033f444b91a5c536c11eee61ff32bdc84caf1172
bryantserrano96 <42443238+bryantserrano96@users.noreply.github.com>
2018-12-17 07:50:14 -0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,74 +1,148 @@
-AST = []
-#program,function,statement,expression
+#to install the anytree module run: $ pip install anytree
+from anytree import Node, RenderTree
 
-def parseUnaryOp( listTokens, ast = AST ):
+unaryOp     =   ["Minus","LogicalNegation"]
+binaryOp    =   ["Plus","Minus","Mult","Div"] 
+
+#function to add new node
+def addT( name, par = None ):
+        #case node doesn't have a parent
+        if par == None: aux = Node( name, parent = None )
+        #other case; "has a parent"
+        else: aux = Node( name, parent = par )
+        #return Node aux
+        return aux
+
+#function to print Tree with givin root
+def printT( root ):
+    for pre,fill,node in RenderTree( root ):
+        print( "%s%s" % ( pre, node.name ) )
+
+
+def parseBinOp( listTokens ):
+    if listTokens[0] == "Plus":
+        Sum = addT( "+" )
+        return Sum, listTokens[1:]
+
+    if listTokens[0] == "Minus":
+        Minus = addT( "-" )
+        return Minus, listTokens[1:]
+
+    if listTokens[0] == "Mult":
+        Mult = addT( "*" )
+        return Mult, listTokens[1:]
+
+    if listTokens[0] == "Div":
+        Div = addT( "/" )
+        return Div, listTokens[1:]
+
+def parseUnaryOp( listTokens ):
     if( listTokens[0] == "LogicalNegation" ):
-        ast.insert( 0, "!" )
+        Ln = addT( "!" )
+        return Ln, listTokens[1:]
 
-    return listTokens[1:]
+    if( listTokens[0] == "Minus" ):
+        Mi = addT( "-" )
+        return Mi, listTokens[1:]
+   
+def parseExp( listTokens ):
 
-def parseExp( listTokens, ast = AST ):
     if( not listTokens ):
-        print("Error: Mising expression ")
+        print("Parser Error: Mising expression ")
         exit()
+    
+    if listTokens[0] in unaryOp: 
+
+        unOp, rest = parseUnaryOp( listTokens )
+
+        node,rest = parseExp( rest )
+
+        if node:
+            unOp.parent = node
+    
+        return node, rest
+
+    if  "Plus" in listTokens[1:] or "Minus" in listTokens[1:] or "Mult" in listTokens[1:] or "Div" in listTokens[1:]:
+        
+        l = []
+        l.append( list( listTokens[0] ) )
+
+        ls, rest = parseExp( l )
+        
+        binOp, rest = parseBinOp( listTokens[1:] )
+
+        rs, rest = parseExp( rest )
+
+        if ls:
+            ls.parent = binOp
+        if rs:
+            rs.parent = binOp
+
+        return binOp, rest
 
     if listTokens[0][0] == "Const" : 
-        ast.insert( 0, listTokens[0][1] )
-        return listTokens[1:]
-    elif listTokens[0] == "LogicalNegation": 
-        unOp = parseUnaryOp( listTokens,ast )
-        parseExp( unOp,ast )
-        return unOp[1:]
+    
+        Cte = addT( int( listTokens[0][1] ) )
+        return Cte, listTokens[1:]
 
-def parseState( listTokens, ast = AST ):
+    else:
+        print( "Parser Error: Missing Expression")
+        exit()
+
+def parseState( listTokens ):
     if( not listTokens ):
-        print("Error: no tokens")
+        print( "Parser Error: No tokens to parse" )
         exit()
 
     if( listTokens[0][0] != "KW" and listTokens[0][1] != "return" ): 
-        print("Error: no statement")
+        print( "Parser Error: No statement" )
         exit()
-
-    exp = parseExp( listTokens[1:], ast )
-
-    if( exp ):
-        if( not exp[0] ):
-            print("Error: Mising keyword: ';' ")
-            exit()
-
-    ast.insert( 0, "Return" )
     
-    return exp.pop()
+    node, rest = parseExp( listTokens[1:] )
+    
+    if( rest[0] != "SemiC" ):
+        print( "Parser Error: Mising keyword: ';'" )
+        exit()
+    
+    Re = addT( "Return" )
 
-def parseFun( listTokens, ast = AST ):
+    node.parent = Re
+
+    return Re, rest[1:]
+
+def parseFun( listTokens ):
     if( not listTokens or listTokens[0][0] != "KW" or listTokens[0][1] != "int" ):
-        print("Error: Missing keyword: int")
+        print( "Parser Error: Missing keyword: int" )
         exit()
     if( listTokens[1][0] != "ID" ):
-        print("Error: Mising Function's name" )
+        print( "Parser Error: Mising Function's name" )
         exit()
     if( listTokens[2] != "OpenPa" ):
-        print("Error: Mising token: '(' ")
+        print( "Parser Error: Mising token: '('" )
         exit()
     if( listTokens[3] != "ClosePa" ):
-        print("Error: Mising token: ')' ")
+        print( "Parser Error: Mising token: ')' " )
         exit()
     if( listTokens[4] != "OpenBr" ): 
-        print("Error: Mising token: '{' ")
+        print( "Parser Error: Mising token: '{' " )
         exit()
-    state = parseState( listTokens[5:], ast )
+        
+    node, rest = parseState( listTokens[5:] )
     
-    if( state != "CloseBr" ):
-        print("Error: Mising token: '}' ")
+    if( rest[0] != "CloseBr" ):
+        print( "Parser Error: Mising token: '}' " )
         exit()
 
-    ast.insert( 0, listTokens[1][1] )
+    Fun = addT( "Function" )
+    node.parent = Fun    
+    return Fun
 
-def parseProg( listTokens, ast = AST  ):
-    if( not listTokens ): return "Error: Missing Tokens"
+def parseProg( listTokens ):
+    if( not listTokens ): return "Parser Error: Missing Tokens"
 
-    fun = parseFun( listTokens, ast )
+    node = parseFun( listTokens )
+    if node:
+        Prog = addT( "Program" )
+        node.parent = Prog
 
-    ast.insert( 0, "Program" )
-
-    return ast
+    return Prog
