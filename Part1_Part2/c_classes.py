8f14ffe6b494066c2c0f354aa96f8aa45548c9e9
Gerardo Arjona <gerardo.arj15@gmail.com>
2019-01-02 13:31:27 -0600

    Fixed ass generation Part1, Add token classes Part2
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

---
+++ Part1_Part2/c_classes.py
@@ -0,0 +1,47 @@
+class Int_kw:
+    def __init__(self):
+        self.kw = "int"
+
+class Id_kw:
+    def __init__(self, id):
+        self.id = id
+
+class Open_paren:
+    def __init__(self):
+        self.paren = "("
+
+class Close_paren:
+    def __init__(self):
+        self.paren = ")"
+    
+class Open_brace:
+    def __init__(self):
+        self.brace = "{"
+
+class Close_brace:
+    def __init__(self):
+        self.brace = "}"
+
+class Ret_kw:
+    def __init__(self):
+        self.kw = "return"
+
+class Literal_num:
+    def __init__(self, num):
+        self.num = num
+
+class Semicolon:
+    def __init__(self):
+        self.semi = ";"
+
+class Negation:
+    def __init__(self):
+        self.neg = "-"
+
+class BitwiseComplement:
+    def __init__(self):
+        self.comp = "~"
+
+class LogicalNegation:
+    def __init__(self):
+        self.neg = "!"
---
+++ Part1_Part2/parser.py
@@ -0,0 +1,100 @@
+import re
+import os
+
+from c_classes import *
+
+from lex_function import lex
+
+def expression(tokens):
+    num=[]
+    for t in tokens:
+        if  isinstance(t, Literal_num):
+            num.append(t)
+            break
+    return num
+
+def statement(tokens):
+    stat=[]
+    for t in tokens:
+        if  isinstance(t, Ret_kw):
+            stat.append(t)
+            tokens.remove(t)
+            stat.append(expression(tokens))
+        elif isinstance(t, Semicolon):
+            tokens.remove(t)
+    return stat
+
+def program(tokens):
+    p=[]
+    for t in tokens:
+        if isinstance(t, Id_kw):
+            p.append(t)
+            tokens.remove(t)
+        elif isinstance(t, Open_paren):
+            tokens.remove(t)
+        elif isinstance(t, Close_paren):
+            tokens.remove(t)
+        elif isinstance(t, Open_brace):
+            tokens.remove(t)
+        elif isinstance(t, Close_brace):
+            tokens.remove(t)
+        else:
+            p.append(statement(tokens))
+            break
+    return p
+
+def parse(tokens):
+    ast=[]
+    for t in tokens:
+        if isinstance(t, Int_kw):
+            ast.append(t)
+            tokens.remove(t)
+        else:
+            ast.append(program(tokens))
+            break
+    
+    print("----- AST -----")
+    print(ast)
+    print()
+    return ast
+
+def iterate(l):
+    python_comp = open("python_comp.s","a+")
+    for i in l:
+        if type(i) == list:
+            iterate(i)
+            python_comp.close()
+        else:
+            if isinstance(i, Int_kw):
+                int_kw = i.kw
+                print("FUN RETURN TYPE: "+str(int_kw))
+            elif isinstance(i, Id_kw):
+                id_kw = i.id
+                python_comp.write(' .globl _{}\n'.format(i.id))
+                python_comp.write('_{}:\n'.format(i.id))
+                print("\t FUN NAME: "+str(id_kw))
+                python_comp.close()
+            elif isinstance(i, Ret_kw):
+                ret = i.kw
+                print("\t\t FUN BODY:")
+                print("\t\t\t"+str(ret)+" ", end='')
+                if isinstance(l[l.index(i)+1][0], Literal_num):
+                    n = l[l.index(i)+1][0].num
+                    python_comp.write(' movl  ${}, {}eax\n'.format(n, "%"))
+                    python_comp.write('  ret\n')
+                    print(str(n), end='')
+                    python_comp.close()
+                    break
+            else:
+                print("Error: AST badly constructed")
+                break
+            
+
+def generate(ast):
+    print("----Pretty AST----")
+    iterate(ast)
+    os.system('gcc python_comp.s -o out')
+    os.remove("python_comp.s")
+    print("\n\n\n==========NOTA==================>Ejecute los siguientes comandos para ver el resultado del compilador: \n1: ./out \n2: echo $?")
+
+generate(parse(lex()))
