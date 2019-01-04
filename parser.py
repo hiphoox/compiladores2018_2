41a1ecf372abdcd660b73590552e5749aeed7184
tanagromo <tanagromo@gmail.com>
2019-01-03 19:40:56 -0600

    add unary operators parser

--- parser.py
+++ parser.py
@@ -11,72 +11,64 @@ from tokenizer import *
 def unOp(tokens):    
     un = []
     print(tokens)
-    for i in tokens:
-        l=i.split(":")
-        if l[1] == "-" or l[1] == "~" or l[1] == "!":
-            un.append(l[1])
-            print(un)
-            return un
+    l=tokens.pop(0).split(":")
+    if l[1] == "-" or l[1] == "~" or l[1] == "!":
+        un.append(l[1])
+        un.append(expresion(tokens))
+        return un
     
 
 def expresion(tokens):
     exp = []
-    for i in tokens:
-        l=i.split(":")
-        if l[0] ==  "INT":
-            exp.append(l[1])
-            tokens.remove(i)
-        elif l[0] == "Negation" or l[0] == "Bitwise" or l[0] == "Logic_Neg":
-            exp.append(unOp(tokens))
-
-       # elif l[0] == "!" or "~" or "-":
-            
+    print(tokens)
+    l=tokens[0].split(":")
+    if l[0] ==  "INT":
+        exp.append(l[1])
+        tokens.pop(0)
+    else:
+        exp.append(unOp(tokens))            
     return exp
 
 def statement(tokens):
     sta = []
-    for i in tokens:
-        l=i.split(":")
-        if l[0] ==  "Keyword" and l[1] == "return":
-            sta.append(l[1])
-            sta.append(expresion(tokens))
-        elif l[0] ==  "Semicolon":
-            sta.append(l[1])
-            pass
+    l=tokens.pop(0).split(":")
+    if l[0] ==  "Keyword" and l[1] == "return":
+        sta.append(l[1])
+        sta.append(expresion(tokens))
+    l=tokens.pop(0).split(":")
+    if l[0] ==  "Semicolon":
+        sta.append(l[1])
     return sta
+    
 
 def func(tokens):
     aidi = []
-    for i in tokens:
-        l=i.split(":")
-        if l[0] ==  "ID":
-            aidi.append(l[1])
-        elif l[0] ==  "OpenParen":
-            aidi.append(l[1])
-            pass
-        elif l[0] ==  "CloseParen":
-            aidi.append(l[1])
-            pass
-        elif l[0] ==  "OpenBrace":
-            aidi.append(l[1])
-            aidi.append(statement(tokens))
-            pass
-        elif l[0] ==  "CloseBrace":
-            aidi.append(l[1])
-            pass
+    l=tokens.pop(0).split(":")
+    if l[0] ==  "ID":
+        aidi.append(l[1])
+    l=tokens.pop(0).split(":")
+    if l[0] ==  "OpenParen":
+        aidi.append(l[1])
+    l=tokens.pop(0).split(":")
+    if l[0] ==  "CloseParen":
+        aidi.append(l[1])
+    l=tokens.pop(0).split(":")
+    if l[0] ==  "OpenBrace":
+        aidi.append(l[1])
+        aidi.append(statement(tokens))
+    l=tokens.pop(0).split(":")
+    if l[0] ==  "CloseBrace":
+        aidi.append(l[1])
     return aidi
+    
 
 def programa(tokens):
     fun = []
-    for i in tokens:
-        l=i.split(":")
-        if l[0] ==  "Keyword" and l[1] == "int":
-            fun.append(l[1])
-            tokens.remove(i)
-        else:
-            fun.append(func(tokens))
-            break
-    return fun        
+    l=tokens.pop(0).split(":")
+    if l[0] ==  "Keyword" and l[1] == "int":
+        fun.append(l[1])
+        fun.append(func(tokens))
+    return fun      
 
 
 
